78cbd09f624f084efc2a650a9612c79a110f8508
Bufal8 <125837797+Bufal8@users.noreply.github.com>
2023-10-19 14:13:33 +0200

    First snack completed

---
+++ src/org/lessons/java/Snack1.java
@@ -0,0 +1,21 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack1 {
+    public static void main(String[] args) {
+        Scanner numScan = new Scanner(System.in);
+        System.out.println("Inserisci un numero: se pari lo stamperò, se dispari stamperò il numero pari successivo. Terminerò l'esecuzione quando inserirai un numero negativo. ");
+        int userNumber = numScan.nextInt();
+        while (userNumber > 0){
+            if (userNumber % 2 == 0) {
+                System.out.println(userNumber);
+            } else {
+                System.out.println(userNumber + 1);
+            }
+            System.out.println("Inserisci un altro numero: ");
+            userNumber = numScan.nextInt();
+        }
+
+    }
+}
